f96d504d5282c85680b70809c7786cb18798e0d8
Immaculate Nyoni <sthwaloe@gmail.com>
2025-11-07 11:34:27 +0200

    chore: cleanup build.gradle.kts configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,12 +87,9 @@ checkstyle {
 spotbugs {
     toolVersion = "4.8.3"
     excludeFilter = rootProject.file("config/spotbugs/exclude.xml")
-    // Temporarily disabled due to EI_EXPOSE_REP issues in model classes
-    // TODO: Re-enable after fixing model class encapsulation
     ignoreFailures = true
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
